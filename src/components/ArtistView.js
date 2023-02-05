5bc6cea016f86d6759f350942eba5592dc4fae6d
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2023-02-04 22:38:22 -0500

    utilized components

---
+++ src/components/ArtistView.js
@@ -0,0 +1,13 @@
+import {useState} from 'react'
+import { useParams } from 'react-router-dom'
+
+export default function ArtistView() {
+    const {id} = useParams()
+    const[artistData, setArtistData] = useState([])
+  return (
+    <div>
+        <h2>The id passed was: {id}</h2>
+        <p> Artist data goes here </p>
+    </div>
+  )
+}
